6e4b7466915fe2c3f6aa237f4c1811ffff08d9d9
alexstar <astarcevmail@gmail.com>
2016-08-30 21:48:07 +0300

    keyListener

--- script.js
+++ script.js
@@ -57,39 +57,47 @@ function arrowListener() {
   setCellValue(row,col,true);
 }
 
+//
+//
+//
+function keyListener(row,col) {
+  setCellValue(row,col,false);
+  if (event.keyCode == 37) {
+    if (col!=0) {
+      col--;
+    } else {
+      col=COLS - 1;
+    }
+  } else if (event.keyCode == 38) {
+    if (row !=0) {
+      row--;
+    } else {
+      row = ROWS - 1;
+    }
+  } else if (event.keyCode == 39) {
+    if (col != COLS - 1) {
+      col++;
+    } else {
+      col = 0;
+    }
+  } else if (event.keyCode == 40) {
+    if (row != ROWS - 1){
+      row++;
+    } else {
+      row = 0;
+    }
+  }
+  setCellValue(row,col,true);
+}
+
+//
+//Запускает игру
+//
 function play() {
   var row = 0;
   var col = 0;
   setCellValue(row,col,true);
-  addEventListener('keydown', function(event) {
-    setCellValue(row,col,false);
-    if (event.keyCode == 37) {
-      if (col!=0) {
-        col--;
-      } else {
-        col=COLS - 1;
-      }
-    } else if (event.keyCode == 38) {
-      if (row !=0) {
-        row--;
-      } else {
-        row = ROWS - 1;
-      }
-    } else if (event.keyCode == 39) {
-      if (col != COLS - 1) {
-        col++;
-      } else {
-        col = 0;
-      }
-    } else if (event.keyCode == 40) {
-      if (row != ROWS - 1){
-        row++;
-      } else {
-        row = 0;
-      }
-    }
-    setCellValue(row,col,true);
-  });
+  addEventListener('keydown', keyListener);
 }
 
 
