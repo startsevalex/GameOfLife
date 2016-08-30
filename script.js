09df3857a3f627e952509073ebb6e6b4c6a169ef
alexstar <astarcevmail@gmail.com>
2016-08-30 15:36:00 +0300

    Constants, comments

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+var ROWS = 32;
+var COLS = 32;
 
+var ROWSTART = 0;
+var COLSTART = 0;
+
+//
+//создает игровое поле
+//
 function createMatrix() {
   var matrix = document.getElementById('matrix');
-  var n = 32 * 32;
+  var n = ROWS * COLS;
   for (var i = 0; i < n; i++) {
     var div = document.createElement('div');
     div.className = 'cell';
@@ -9,17 +17,26 @@ function createMatrix() {
   }
 }
 
+//
+//возвращает div (ячейку)
+//
 function getCell(row,col) {
-  var index = col+row*32;
+  var index = col+row*COLS;
   var cell = document.getElementById('matrix').children[index];
   return cell;
 }
 
+//
+//устанавливает значение ячейки (закарашена или нет)
+//
 function setCellValue(row,col,val) {
   var cell = getCell(row,col);
   cell.style.backgroundColor = (val == true) ? 'blue' : 'inherit';
 }
 
+//
+//возвращает состояние ячейки (закрашена или нет)
+//
 function getCellValue(row,col) {
   var cell = getCell(row,col);
   var cellColor = cell.style.backgroundColor;
