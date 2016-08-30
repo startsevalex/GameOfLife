162964001a0755e825ac90f5b0e512151b1a09ad
alexstar <astarcevmail@gmail.com>
2016-08-30 21:33:43 +0300

    Looped world

--- script.js
+++ script.js
@@ -61,26 +61,40 @@ function play() {
   var row = 0;
   var col = 0;
   setCellValue(row,col,true);
-  addEventListener('keydown', arrowListener);
+  addEventListener('keydown', function(event) {
+    setCellValue(row,col,false);
+    if (event.keyCode == 37) {
+      if (col!=0) {
+        col--;
+      } else {
+        col=COLS - 1;
+      }
+    } else if (event.keyCode == 38) {
+      if (row !=0) {
+        row--;
+      } else {
+        row = ROWS - 1;
+      }
+    } else if (event.keyCode == 39) {
+      if (col != COLS - 1) {
+        col++;
+      } else {
+        col = 0;
+      }
+    } else if (event.keyCode == 40) {
+      if (row != ROWS - 1){
+        row++;
+      } else {
+        row = 0;
+      }
+    }
+    setCellValue(row,col,true);
+  });
 }
 
 
 window.onload = function() {
   createMatrix();
-  var row = 0;
-  var col = 0;
-  setCellValue(row,col,true);
-  addEventListener('keydown', function(event) {
-    setCellValue(row,col,false);
-    if (event.keyCode == 37) {
-      col--;
-    } else if (event.keyCode == 38) {
-      row--;
-    } else if (event.keyCode == 39) {
-      col++;
-    } else if (event.keyCode == 40) {
-      row++;
-    }
-    setCellValue(row,col,true);
-  });
+  play();
+
 }
